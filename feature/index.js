6f53d70e4ccda5824447996d0ad0e53b55e05dc2
Aidan98 <aidanovic22@outlook.com>
2019-06-04 13:59:47 +0200

    Update index.js

--- feature/index.js
+++ feature/index.js
@@ -1,24 +1,16 @@
-// var express = require('');
-// var app = express();
-// app.get('helloThere', function(request, response){
-//     response.send('hello there, from espress!')
-// });
+const express = require("express");
+const path = require("path");
+const app = express();
 
-// app.listen(1444, function(){
-//     console.log('Listening at Port 1444');
-// });
+// // Static folder
+// app.use(express.static(path.join(__dirname, "views")));
 
-var coll = document.getElementsByClassName("collapseExpand");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+res.render("index", {data : { filters : ["Action", "Adventure", "Comedy"]}});
+});
+
+app.listen(1444, function(){
+    console.log('Listening at Port 1444');
+});
